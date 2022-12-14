e8473571183515d573bbcf55bc06760d2621bc03
Bobby Morck <rmorck3098@gmail.com>
2022-12-14 01:57:36 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './fonts/muli.light.ttf';
+import Fader from './components/Fader';
+import IntroText from './components/IntroText';
+import Typewriter from 'typewriter-effect'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Fader text="Bobby Morck"></Fader>
+      <IntroText/>
+    </>
   );
 }
 
