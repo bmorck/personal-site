a1c1561716285e7cae9b11195127de8d771d9a65
bmorck <rmorck3098@gmail.com>
2022-12-24 02:13:12 -0500

    remove unused code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="flex flex-row justify-center space-x-2">
                 <SocialIcon style={{ height: 25, width: 25 }} className="pr-1" bgColor="#9c9c9c" url="https://www.linkedin.com/in/bobbymorck/" />
                 <SocialIcon style={{ height: 25, width: 25 }} className="" bgColor="#9c9c9c" url="https://github.com/bmorck" />
-                <SocialIcon style={{ height: 25, width: 25 }} className="pr-1" bgColor="#9c9c9c" url="rmorck3098@gmail.com" network="email" />
+                <SocialIcon style={{ height: 25, width: 25 }} className="pr-1" bgColor="#9c9c9c" url="mailto:rmorck3098@gmail.com" />
                 <SocialIcon style={{ height: 25, width: 25 }} className="pr-1" bgColor="#9c9c9c" url="https://www.facebook.com/bobby.morck" />
                 <SocialIcon style={{ height: 25, width: 25 }} className="pr-1" bgColor="#9c9c9c" url="https://medium.com/@rmorck3098" />
 
