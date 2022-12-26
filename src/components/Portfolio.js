f93dc3e803bc871625a52842def416d27134a943
bmorck <rmorck3098@gmail.com>
2022-12-26 02:49:04 -0500

    add border to cards and add padding and fix link

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,14 +7,14 @@ const Portfolio = () => {
             <div className="md:basis-1/3"></div>
             <div className="md:basis-1/2 pl-5 pr-5 drop-shadow-md w-11/12">
                 {/* Card for RL Project */}
-                <div className="flex flex-col md:flex-row items-center border-0 drop-shadow-sm rounded-lg bg-custom-bg-color2 p-5 mb-5">
+                <div className="flex flex-col md:flex-row items-center border-0 drop-shadow-sm rounded-lg bg-custom-bg-color2 p-5 mb-5 border-1">
                     {/*<div className="mr-5 ml-5 w-36 border-1 drop-shadow-lg hover:opacity-75 transition ease-in-out delay-50 duration-500 relative hover:scale-110">
                         <div class=" inset-0 bg-cover z-0 "> <img src={rlenv} alt="RL Env" /></div>
                         <div className="opacity-0 hover:opacity-100 duration-500 absolute inset-0 z-10 flex justify-center items-center text-sm text-white font-display-font font-semibold underline">&lt; View Code &gt;</div>
                      </div>*/}
 
                     <div
-                        class="overflow-hidden mr-5 ml-5 w-36 shrink-0 rop-shadow-md rounded-md relative group hover:opacity-75"
+                        class="overflow-hidden mr-5 ml-5 mb-5 md:mb-0 w-36 shrink-0 rop-shadow-md rounded-md relative group hover:opacity-75"
                     >
                         <div
                             class="z-50 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out absolute from-black/80 to-transparent bg-gradient-to-t inset-0 flex justify-center items-center"
@@ -22,7 +22,7 @@ const Portfolio = () => {
                             <div
                                 class="transform-gpu text-sm group-hover:opacity-100 group-hover:translate-y-0 translate-y-4 transform transition duration-500 ease-in-out"
                             >
-                                <a href="https://github.com/bmorck/TigerTravel" class="font-bold underline text-stone-300 font-display-font">&lt; View Source &gt;</a>
+                                <a href="https://github.com/bmorck/MultiCar-RL" class="font-bold underline text-stone-300 font-display-font">&lt; View Source &gt;</a>
                             </div>
 
                         </div>
@@ -53,7 +53,7 @@ const Portfolio = () => {
                 </div>
 
                 {/* Card for TigerTravel Project */}
-                <div className="flex flex-col md:flex-row items-center border-0 drop-shadow-sm rounded-lg bg-custom-bg-color2 p-5 mb-5">
+                <div className="flex flex-col md:flex-row items-center border-0 drop-shadow-sm rounded-lg bg-custom-bg-color2 p-5 mb-5 border-1">
                     <div className="mr-5 ml-5 w-36 shrink-0 mx-auto">
 
                     </div>
@@ -78,7 +78,7 @@ const Portfolio = () => {
                 </div>
 
                 {/* Card for TigersInProduct Project */}
-                <div className="flex flex-col md:flex-row items-center border-0 drop-shadow-sm rounded-lg bg-custom-bg-color2 p-5 mb-5">
+                <div className="flex flex-col md:flex-row items-center border-0 drop-shadow-sm rounded-lg bg-custom-bg-color2 p-5 mb-5 border-1">
                     <div className="mr-5 ml-5 w-36 shrink-0 mx-auto">
 
                     </div>
@@ -103,7 +103,7 @@ const Portfolio = () => {
                 </div>
 
                 {/* Card for Website Project */}
-                <div className="flex flex-col md:flex-row items-center border-0 drop-shadow-sm rounded-lg bg-custom-bg-color2 p-5 mb-5">
+                <div className="flex flex-col md:flex-row items-center border-0 drop-shadow-sm rounded-lg bg-custom-bg-color2 p-5 mb-5 border-1">
                     <div className="mr-5 ml-5 w-36 shrink-0 mx-auto">
 
                     </div>
@@ -128,7 +128,7 @@ const Portfolio = () => {
                 </div>
 
                 {/* Card for TriGrid Painter Project */}
-                <div className="flex flex-col md:flex-row items-center border-0 drop-shadow-sm rounded-lg bg-custom-bg-color2 p-5 mb-5">
+                <div className="flex flex-col md:flex-row items-center border-0 drop-shadow-sm rounded-lg bg-custom-bg-color2 p-5 mb-5 border-1">
                     <div className="mr-5 ml-5 w-36 shrink-0 mx-auto">
 
                     </div>
@@ -153,7 +153,7 @@ const Portfolio = () => {
                 </div>
 
                 {/* Card for Uber Driver Concurrency Project */}
-                <div className="flex flex-col md:flex-row items-center border-0 drop-shadow-sm rounded-lg bg-custom-bg-color2 p-5 mb-5">
+                <div className="flex flex-col md:flex-row items-center border-0 drop-shadow-sm rounded-lg bg-custom-bg-color2 p-5 mb-5 border-1">
                     <div className="mr-5 ml-5 w-36 shrink-0 mx-auto">
 
                     </div>
