70957c8ed5be381d75202dce4f6db36eed81dca2
bmorck <rmorck3098@gmail.com>
2022-12-27 23:01:26 -0500

    add more to portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,6 +4,7 @@ import tigertravel from "../files/tigertravel.png"
 import tip from "../files/TigersInProduct.png"
 import bmorck from "../files/bmorck.png"
 import mongosocial from "../files/mongosocial.png"
+import concurrency from "../files/concurrency.png"
 import rlreport from "../files/RLreport.pdf"
 
 const Portfolio = () => {
@@ -94,9 +95,9 @@ const Portfolio = () => {
                         </div>
 
                         <div className="text-lg">
-                            <div className="font-bold text-center md:text-left pb-2">Tiger Travel</div>
+                            <div className="font-bold text-center md:text-left pb-2">TigerTravel</div>
                             <div className="text-xs">
-                                Tiger Travel is a webapp that helped Princeton students connect and form groups
+                                TigerTravel is a webapp that helped Princeton students connect and form groups
                                 for ride-sharing applications based on overlapping time intervals and destinations.
                                 Users communicated using in-app messaging to coordinate with group members.
                             <br />
@@ -306,27 +307,49 @@ const Portfolio = () => {
                 </div>
 
                 {/* Card for Uber Driver Concurrency Project */}
-                <div className="flex flex-col md:flex-row items-center border-0 drop-shadow-sm rounded-lg bg-custom-bg-color2 p-5 mb-5 border-1">
-                    <div className="mr-5 ml-5 w-36 shrink-0 mx-auto">
+                <div className="flex flex-col  border-0 drop-shadow-sm rounded-lg bg-custom-bg-color2 p-5 mb-5 border-1">
+                    <div className="flex flex-col md:flex-row items-center">
+                        {/*<div className="mr-5 ml-5 w-36 border-1 drop-shadow-lg hover:opacity-75 transition ease-in-out delay-50 duration-500 relative hover:scale-110">
+                        <div class=" inset-0 bg-cover z-0 "> <img src={rlenv} alt="RL Env" /></div>
+                        <div className="opacity-0 hover:opacity-100 duration-500 absolute inset-0 z-10 flex justify-center items-center text-sm text-white font-display-font font-semibold underline">&lt; View Code &gt;</div>
+                     </div>*/}
 
+                        <div
+                            class="overflow-hidden mr-5 ml-5 mb-5 md:mb-0 w-36 shrink-0 rop-shadow-md rounded-md relative group hover:opacity-75 transition duration-500 ease-in-out"
+                        >
+                            <div
+                                class="z-50 opacity-0 group-hover:opacity-100 transition duration-500 ease-in-out absolute from-black/80 to-transparent bg-gradient-to-t inset-0 flex justify-center items-center"
+                            >
+                                <div
+                                    class="transform-gpu text-sm group-hover:opacity-100 group-hover:translate-y-0 translate-y-4 transform transition duration-500 ease-in-out"
+                                >
+                                    <div className=""><a href="https://github.com/bmorck/personal-site" class="font-code2 rounded-xl p-1 bg-opacity-75 bg-orange-200 ">&lt; View Source &gt;</a> </div>
+                                </div>
+
+                            </div>
+                            <img
+                                alt=""
+                                class="inset-0 scale-75 group-hover:scale-100 transition duration-500 ease-in-out"
+                                src={concurrency}
+                            />
+                        </div>
+
+                        <div className="text-lg">
+                            <div className="font-bold text-center md:text-left pb-2">Concurrency Snippets</div>
+                            <div className="text-xs">
+                                Due to my current role as a backend engineer, I built this site to re-expose
+                                myself to frontend development and gain experience in reactJS. I also wanted
+                                to have a central, easily accessible place where others can view my
+                                work and where I can share my thoughts.
+                            <br />
+                            </div>
+
+                        </div>
                     </div>
-
-                    <div className="text-lg font-regular-font">
-                        <div className="font-bold text-center md:text-left pb-2">Various Concurreny Demos</div>
-                        <div className="text-sm">
-
-                        </div>
-                        <div className="flex pt-2 flex-wrap">
-                            <div className="border-1 text-sm p-1 mr-4 mt-2 rounded-xl border-sky-200 bg-cyan-50 text-gray-500">
-                                Python
+                    <div className="flex pt-2 flex-wrap justify-center">
+                        <div className="border-1 text-sm p-1 mr-4 mt-2 rounded-xl border-sky-200 bg-cyan-50 text-gray-500">
+                            Golang
                             </div>
-                            <div className="border-1 text-sm p-1 mr-4 mt-2 rounded-xl border-sky-200 bg-cyan-50 text-gray-500">
-                                PyGame
-                            </div>
-                            <div className="border-1 text-sm p-1 mr-4 mt-2 rounded-xl border-sky-200 bg-cyan-50 text-gray-500">
-                                RLlib
-                            </div>
-                        </div>
                     </div>
                 </div>
 
